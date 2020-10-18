0aa5ada40b3211370f4382fcf90cade3f7adfa11
Juheeshwari <Juhi@INIADMINs-MacBook-Pro.local>
2020-10-18 02:00:39 -0700

    Basic content changes done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+  "imagebaseurl":"https://www.linkedin.com/in/juheeshwari-laxmi-rajamohan-45a947126/",
+  "name": "Juheeshwari",
+  "role": "Full-stack web developer",
+  "linkedinId":"Your LinkedIn Id",
+  
+  "roleDescription": "I like dabbling in various parts of web development and like to learn about new technologies and occasionally write techical blogs.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/juheeshwari-laxmi-rajamohan-45a947126/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/juhi16792",
+        "className":"fa fa-github"
+      },
+      
+    ],
+  "aboutme":"I enjoy turning complex problems into simple and intuitive designs. When I'm not coding, tweeting or pushing pixels, you'll find me cooking, gardening or working out in the park.",
+  "education":[
+    {
+      "UniversityName":"California State Univeristy,Los Angeles",
+      "specialization":"Master of science",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2018",
+    },
+    {
+      "UniversityName":"Gujarat Technological University",
+      "specialization":"Bachelor of Engineering",
+      "MonthOfPassing":"Jun",
+      "YearOfPassing":"2014",
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"University of Southern California",
+      "specialization":"Web developer"
+    },
+    {
+      "CompanyName":"JLL",
+      "specialization":"Data Analyst",
+      "MonthOfLeaving":"Sep",
+      "YearOfLeaving":"2018"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Institute of Nueroimaging",
+      "description":"Website",
+      "url":"https://github.com/juhi16792",
+      "className":"fa fa-github",
+      "imgurl":"images/portfolio/phone1.jpg"
+     
+    },
+    {
+      "name":"Labrotory of neuro Imagining",
+      "description":"Website",
+      "imgurl":"images/portfolio/project3.jpg",
+      "url":"https://github.com/juhi16792"
+     
+    },
+    {
+      "name":"Covid-arc",
+      "description":"Website",
+      "imgurl":"images/portfolio/project4.jpg",
+      "url":"https://github.com/juhi16792"
+     
+    },
+    {
+      "name":"CIC",
+      "description":"Website",
+      "imgurl":"images/portfolio/project5.jpg",
+      "url":"https://github.com/juhi16792"
+     
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
