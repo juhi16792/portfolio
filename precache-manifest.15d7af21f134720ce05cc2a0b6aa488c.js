f8ccd72d5e292890daa2a2a404a65381775cc846
Juheeshwari <Juhi@INIADMINs-MacBook-Pro.local>
2020-10-20 00:29:55 -0700

    Updates

---
+++ precache-manifest.15d7af21f134720ce05cc2a0b6aa488c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59196b4cbb83f82db5aef65f66aec40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eb2ba4f1661b09fe2b8",
+    "url": "/static/css/main.7db9598a.chunk.css"
+  },
+  {
+    "revision": "33e6a020f9efaa4e46b3",
+    "url": "/static/js/2.c5444ead.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.c5444ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb2ba4f1661b09fe2b8",
+    "url": "/static/js/main.622642aa.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
