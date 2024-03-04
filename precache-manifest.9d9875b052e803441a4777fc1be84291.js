7ee425c56f4d285be0bbc1a04be18b831ee186a3
Juheeshwari Rajmohan <juhi@Juheeshwaris-MacBook-Air.local>
2024-03-03 20:53:36 -0800

    Updates

---
+++ precache-manifest.9d9875b052e803441a4777fc1be84291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5695172cbcefa6ae5956fe4dd91a03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f202ec667b3672988135",
+    "url": "/static/css/main.7db9598a.chunk.css"
+  },
+  {
+    "revision": "fbb4e0c7bd741cfc7351",
+    "url": "/static/js/2.c56b5c2c.chunk.js"
+  },
+  {
+    "revision": "88a1e5fde3fbde07a43dac26e09075cb",
+    "url": "/static/js/2.c56b5c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f202ec667b3672988135",
+    "url": "/static/js/main.2b02c4d3.chunk.js"
+  },
+  {
+    "revision": "9782e4b201e91e52ccd4",
+    "url": "/static/js/runtime-main.e4c0243c.js"
+  }
+]);
